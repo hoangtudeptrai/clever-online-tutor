11aab364a7e69e4b1ff4bcbe7febbbe1c87f5177
dat-th23 <dat.th1230@gmail.com>
2025-06-11 10:15:40 +0700

    add utils api + component api-url

---
+++ src/components/api-url/index.tsx
@@ -0,0 +1,14 @@
+type ApiEndpoints = {
+    [key: string]: string
+}
+
+export const AUTH_API: ApiEndpoints = {
+    LOGIN: `/auth/login`,
+    REGISTER: `users`
+}
+
+export const USERS_API: ApiEndpoints = {    
+}
+
+export const COURSES_API: ApiEndpoints = {
+}
---
+++ src/utils/api.tsx
@@ -0,0 +1,72 @@
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios';
+
+const BASE_URL = import.meta.env.VITE_API || 'http://127.0.0.1:8080/vms/api/v0';
+
+// Tạo instance axios với config mặc định
+const api = axios.create({
+    baseURL: BASE_URL,
+    headers: {
+        'Content-Type': 'application/json',
+    },
+});
+
+export const METHODS = {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    PATCH: 'PATCH',
+    DELETE: 'DELETE',
+}
+
+async function getToken() {
+    if (typeof window === 'undefined') return ''
+    const token = window.localStorage.getItem('access_token')
+    return token && token.length > 0 ? `Bearer ${token}` : ''
+}
+
+api.interceptors.request.use(async (config) => {
+    const token = await getToken()
+    if (token) {
+        config.headers.Authorization = token
+        config.withCredentials = false
+    }
+
+    return config
+})
+
+api.interceptors.response.use(
+    (response: AxiosResponse) => {
+        return response?.data?.data ? response?.data : response
+    },
+    (error) => {
+        if (error.response?.status === 401 && !window.location.pathname.includes('/login')) {
+            window.location.href = '/login'
+        }
+        return Promise.reject(error)
+    }
+)
+
+api.defaults.withCredentials = false
+
+type Params = Record<string, any>
+type Payload = Record<string, any>
+
+export const getApi = (url: string, params: Params = {}, other: AxiosRequestConfig = {}) => {
+    return api.get(url, { params, ...other })
+}
+
+export const postApi = (url: string, payload: Payload = {}, other: AxiosRequestConfig = {}) => {
+    return api.post(url, payload, { ...other })
+}
+
+export const putApi = (url: string, payload: Payload = {}, other: AxiosRequestConfig = {}) => {
+    return api.put(url, payload, { ...other })
+}
+
+export const patchApi = (url: string, payload: Payload = {}, other: AxiosRequestConfig = {}) => {
+    return api.patch(url, payload, { ...other })
+}
+
+export const deleteApi = (url: string, payload: Payload = {}, other: AxiosRequestConfig = {}) => {
+    return api.delete(url, { data: payload, ...other })
+}
